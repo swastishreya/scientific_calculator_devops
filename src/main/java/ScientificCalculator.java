e1227f7ff04bc5a6ed0aeeb7e23a367ccdd7f37a
swastishreya <mishra.swastishreya13@gmail.com>
2021-03-10 15:26:22 +0530

    added source files for calculator and test

---
+++ src/main/java/ScientificCalculator.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class ScientificCalculator
+{
+    public ScientificCalculator() {
+    }
+
+    public static void main(String args[])
+    {
+        int flag=0,ch;
+        int num1;
+        int num2;
+        ScientificCalculator calculator = new ScientificCalculator();
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Calculator System");
+        do
+        {
+            System.out.println("Option Menu");
+            System.out.println("");
+            System.out.println("1) Addition");
+            System.out.println("5) Exit");
+            System.out.print("Enter your choice: ");
+            ch = reader.nextInt();
+            if(ch==5)
+            {
+                flag = 1;
+            }
+            else
+            {
+                switch(ch)
+                {
+                    case 1:
+                        System.out.println("Addition");
+                        System.out.println("Enter two numbers");
+                        System.out.print("Enter number 1: ");
+                        num1 = reader.nextInt();
+                        System.out.print("Enter number 2: ");
+                        num2 = reader.nextInt();
+                        System.out.println(calculator.add(num1, num2));
+                        break;
+                    default: System.out.println("Exiting program due to invalid input");
+                        flag=1;
+                }
+            }
+            System.out.println("\n");
+        }while(flag==0);
+    }
+    public int add(int number1, int number2) {
+        int result = number1 + number2;
+        return result;
+    }
+}
--- src/test/java/ScientificCalculatorTest.java
+++ src/test/java/ScientificCalculatorTest.java
@@ -1,2 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class ScientificCalculatorTest {
+    ScientificCalculator calculator = new ScientificCalculator();
+
+    @Test
+    public void additionTest(){
+        assertEquals("Adding two integer numbers for True Positive", 4, calculator.add(2, 2));
+        assertEquals("Adding two double numbers for True Positive", 5, calculator.add(2, 3));
+    }
 }
