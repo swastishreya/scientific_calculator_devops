485493d331d08dd6e71227cf30c65c4ff16f6f67
Swasti Shreya Mishra <32703974+swastishreya@users.noreply.github.com>
2021-03-11 16:30:20 +0530

    Update ScientificCalculatorTest.java

--- src/test/java/ScientificCalculatorTest.java
+++ src/test/java/ScientificCalculatorTest.java
@@ -10,4 +10,11 @@ public class ScientificCalculatorTest {
         assertEquals("Adding two double numbers for True Positive", 5, calculator.add(2, 3));
         assertEquals("Adding two double numbers for True Positive", 89, calculator.add(2, 87));
     }
+    
+    @Test
+    public void additionTest2(){
+        assertEquals("Adding two integer numbers for True Positive", 4, calculator.add(2, 2));
+        assertEquals("Adding two double numbers for True Positive", 5, calculator.add(2, 3));
+        assertEquals("Adding two double numbers for True Positive", 89, calculator.add(2, 87));
+    }
 }
